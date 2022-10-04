d588bb3402f3627369884b46498ef9edb4c0a115
Cbutler1 <collin.p.butler1@gmail.com>
2022-10-03 20:38:47 -0400

    Added AddBrew Method

---
+++ Controllers/BrewController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreweryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrewController : ControllerBase
+    {
+        BreweryDBContext _db = new BreweryDBContext();
+
+        [HttpPost("add")]
+        public void AddBrew(string brewName, double abv, string category, 
+        string description, bool isAvailable)
+        {
+            Brew b = new Brew() 
+            { 
+                BrewName = brewName,
+                Abv = abv,
+                Category = category,
+                Description = description,
+                IsAvailable = isAvailable
+            };
+            _db.Brews.Add(b);
+            _db.SaveChanges();
+        }
+
+    }
+}
